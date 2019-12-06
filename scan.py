45cd809c52a7f8ec9ae2fae3dfd763c559af0d0a
Anthony <ab@outofpluto.com>
2019-12-06 12:22:07 +0100

    Version with RSSI and posting to swiv

--- scan.py
+++ scan.py
@@ -1,5 +1,5 @@
-import bluetooth, json
-from blscan import get_nearby_devices, get_rssi_for_devices
+import bluetooth, json, requests
+from blscan import get_nearby_devices#, get_rssi_for_devices
 
 print("Performing inquiry ... ", end='', flush=True)
 
@@ -9,12 +9,13 @@ nearby_devices = {row[0]: {"name": row[1]} for row in nearby_devices}
 print("Found {} devices".format(len(nearby_devices)))
 print("Getting RSSI ... ", end='', flush=True)
 
-get_rssi_for_devices(nearby_devices)
+#get_rssi_for_devices(nearby_devices)
 
 print("Done")
 print("Logging ... ", end='', flush=True)
 
-with open("devices.json", "w") as f:
-    f.write(json.dumps(nearby_devices))
+requests.post("http://swiv.outofpluto.com:5000/api/notify", json.dumps(nearby_devices))
+#with open("devices.json", "w") as f:
+#    f.write(json.dumps(nearby_devices))
 
 print("Done")
